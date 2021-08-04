e31654a339af256b3c0fba71afa5473e836a9efa
d3r1n <derinondereren@gmail.com>
2021-08-04 13:51:28 +0300

    favicon change

--- src/components/Lanyard.vue
+++ src/components/Lanyard.vue
@@ -60,6 +60,10 @@ export default {
 		const quote = await ( await fetch("https://api.quotable.io/random?minLength=100&maxLength=150")).json()
 		this.activity.content = quote.content
 		this.activity.author = quote.author
+
+		let favicon = document.createElement("link").createAttribute("rel", "icon").createAttribute("href", `https://cdn.discordapp.com/avatars/${this.lanyard.discord_user.id}/${this.lanyard.discord_user.avatar}.png?size=512`).createAttribute("type", "image/png")
+		document.head.appendChild(favicon)
+		// <link rel="icon" type="image/png" href="https://example.com/favicon.png"/>
 	},
 	methods: {
 		toggleOpen() {
