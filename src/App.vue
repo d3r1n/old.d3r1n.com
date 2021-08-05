2464e20828ec718e8043aa9d01b6073fb83b7187
d3r1n <derinondereren@gmail.com>
2021-08-05 18:00:02 +0300

    Added Footer Component

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,21 @@
 		<Lanyard />
 		<About />
 	</div>
-
+	<Footer />
 </template>
 
 <script>
 
 import Lanyard from "./components/Lanyard.vue"
 import About from "./components/About.vue"
+import Footer from "./components/Footer.vue"
 
 export default {
   name: 'App',
   components: {
     Lanyard,
     About,
+    Footer,
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="footer">
+		<p>This Website is <a href="https://github.com/d3r1n/d3r1n.com" target="_blank">Open Source</a>. Made with <span id="love">Love</span> by d3r1n. © Derin Önder Eren 2021</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Footer"
+}
+</script>
+
+<style>
+.footer {
+	background-color: #1f1f1f;
+	border: 10px solid #39375b;
+	border-radius: 25px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: fixed;
+	bottom: 0;
+	margin-bottom: 20px;
+	margin-left: 30%;
+	height: 60px;
+	width: 800px;
+}
+
+.footer p {
+	color: #f8f8f2;
+	font-size: 15px;
+	margin-top: 15px;
+
+}
+
+.footer #love {
+	color: #ff5555;
+	font-size: 18px;
+	font-weight: bold;
+}
+
+.footer a {
+	color:#50fa7b;
+	font-weight: bold;
+	font-size: 18px;
+}
+</style>
